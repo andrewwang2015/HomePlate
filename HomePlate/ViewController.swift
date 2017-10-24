2028aaac99054f1acb4319814a6bc13e060192ec
Steven Brotz <sbrotz@Stevens-MacBook-Air.local>
2017-10-24 00:22:42 -0700

    Initial Commit

---
+++ HomePlate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomePlate
+//
+//  Created by Steven Brotz on 10/24/17.
+//  Copyright © 2017 mutegolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
